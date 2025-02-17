33a15d7fa3d7ac86e651db8bf00c04c6d9503d19
Vũ Nhật Duy <vunhatduy285@gmail.com>
2025-02-17 14:56:39 +0700

    moving to new folder

---
+++ video-streaming/src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+const PORT = process.env.PORT;
+
+app.get("/video", (req, res) => {
+
+    const videoPath = path.join("./videos", "SampleVideo_1280x720_1mb.mp4");
+    fs.stat(videoPath, (err, stats) => {
+        if (err) {
+            console.error("An error occurred ");
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+        fs.createReadStream(videoPath).pipe(res);
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Microservice online.`);
+});
