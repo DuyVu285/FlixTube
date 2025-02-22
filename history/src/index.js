924d9db15e4eaf8ac6ab9de909bbe9b8141ee8ec
Vũ Nhật Duy <vunhatduy285@gmail.com>
2025-02-22 19:27:44 +0700

    intergrating rabbitmq

--- history/src/index.js
+++ history/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongodb = require("mongodb");
 const bodyParser = require('body-parser');
+const amqp = require("amqplib");
 
 if (!process.env.DB_HOST) {
     throw new Error("Please specify the databse host using environment variable DBHOST.");
@@ -10,8 +11,13 @@ if (!process.env.DB_NAME) {
     throw new Error("Please specify the name of the database using environment variable DBNAME");
 }
 
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+}
+
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
+const RABBIT = process.env.RABBIT;
 
 function connectDb() {
     return mongodb.MongoClient.connect(DB_HOST)
@@ -19,8 +25,20 @@ function connectDb() {
             return client.db(DB_NAME);
         });
 }
-function setUpHandlers(app, db) {
 
+function connectRabbit() {
+
+    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
+
+    return amqp.connect(RABBIT)
+        .then(messagingConnection => {
+            console.log("Connected to RabbitMQ.");
+
+            return messagingConnection.createChannel();
+        });
+}
+
+function setUpHandlers(app, db, messageChannel) {
 
     const videosCollection = db.collection("videos");
     console.log("Connected to database.");
@@ -56,11 +74,11 @@ function setUpHandlers(app, db) {
     });
 }
 
-function startHttpServer(db) {
+function startHttpServer(db, messageChannel) {
     return new Promise((resolve) => {
         const app = express();
         app.use(bodyParser.json());
-        setUpHandlers(app, db);
+        setUpHandlers(app, db, messageChannel);
 
         const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
         app.listen(port, () => {
@@ -70,9 +88,12 @@ function startHttpServer(db) {
 };
 
 function main() {
-    return connectDb(DB_HOST) 
-        .then(db => {         
-            return startHttpServer(db);
+    return connectDb()
+        .then(db => {
+            return connectRabbit()
+                .then(messageChannel => {
+                    return startHttpServer(db, messageChannel);
+                });
         });
 }
 
